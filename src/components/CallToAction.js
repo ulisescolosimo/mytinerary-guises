58072ff501b209df810a1c6cb6efd1a1e193287d
ulisescolosimo <colosimo101@gmail.com>
2022-08-24 13:23:02 -0300

    Cambio de calltoaction para agregar Link router a cities

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const CallToAction = (props) => {
   return (
-    <a href="#" className="learn-more">
+    <Link to={props.to} className="learn-more">
         <span className="circle" aria-hidden="true">
         <span className="icon arrow"></span>
         </span>
         <span className="button-text">{props.title}</span>
-    </a>
+    </Link>
   )
 }
 
