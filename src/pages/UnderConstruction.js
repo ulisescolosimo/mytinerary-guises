0bfebaf21b2a6df33473b85903d69fda1bd4816f
ulisescolosimo <colosimo101@gmail.com>
2022-08-25 15:37:23 -0300

    Modificamos tamanio de imagen de under construction

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -6,7 +6,7 @@ const UnderConstruction = () => {
       <div className="container-maintenance">
         <h2 class="title-maintenance">Under Construction</h2>
         <p>Easily let website visitors know your site is undergoing maintenance.</p>
-        <img src="./assets/mantenimiento.png" style={{width: "500px", height: "100%"}} alt="trabajando" />
+        <img src="./assets/mantenimiento.png" style={{width: "500px", height: "50%"}} alt="trabajando" />
       </div>
   )
 }
