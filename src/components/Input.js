88977b9682f8fbbf58e88b45101a3d599e2d1e71
ulisescolosimo <colosimo101@gmail.com>
2022-08-28 23:02:01 -0300

    Fix input width

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../styles/NewCities.css'
 
-const Input = ({myRef, name}) => {
+const Input = ({myRef, name, typeText}) => {
 
   return (
             <label className="input-label" htmlFor={name}>{name[0].toUpperCase() + name.substring(1)}
-              <input type="text" className="input-form" id={name} ref={myRef} />
+              <input type={ typeText ==='text' ? 'text' : 'textarea'} name={name} className="input-form" placeholder={`Enter ${name}`} id={name} ref={myRef} />
             </label>  
   )
 }
