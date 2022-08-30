1a0de79e9f8b98ec4fff3d3b1b0dc551a63a44cc
ulisescolosimo <colosimo101@gmail.com>
2022-08-30 13:28:39 -0300

    Modificacion ruta detalles.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import ScrollToTop from './components/ScrollToTop'
 import WebsiteLayout from './layouts/WebsiteLayout'
 import HomePage from './pages/HomePage'
 import Cities from './pages/Cities'
-import UnderConstruction from './pages/UnderConstruction'
 import NewCities from './pages/NewCities'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Error from './pages/Error'
+import CityDetails from './pages/CityDetails'
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/cities" element={<Cities />} />
           <Route path='/new_cities' element={<NewCities />}/>
+          <Route path='/details' element={<CityDetails />} />
           <Route path='*' element={<Error />}/>
         </Routes>
       </WebsiteLayout>
