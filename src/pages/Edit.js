85676c85a61331caf98ee44a275c16c822c63ed1
ulisescolosimo <colosimo101@gmail.com>
2022-09-01 00:19:32 -0300

    Nueva pagina Edit.js

---
+++ src/pages/Edit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EditCity from '../components/EditCity'
+import '../styles/Edit.css'
+
+const Edit = () => {
+  return (
+    <>
+        <EditCity />
+    </>
+  )
+}
+
+export default Edit
