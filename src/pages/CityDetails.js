5aad76027cf824cb21a2870e45662f1c786c0d15
ulisescolosimo <colosimo101@gmail.com>
2022-09-07 18:29:21 -0300

    Modificamos la estructura de city details para llamar nuevos componentes

--- src/pages/CityDetails.js
+++ src/pages/CityDetails.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import Details from '../components/Details'
 import Itinerary from '../components/Itinerary'
 import Comments from '../components/Comments'
 
 const CityDetails = () => {
+
+  const { id } = useParams();
+  const [itinerary, setItinerary] = useState({}) 
+
+  useEffect(() =>{
+        async function fetchData() {
+              const itineraries = await axios.get(`http://localhost:4000/itineraries/?city=${id}`)
+              setItinerary(itineraries.data.response)
+        }
+            fetchData();
+        }, []);
+
   return (
     <>
         <Details />
-        <Itinerary/>
-        
+        <Itinerary itinerary={itinerary} />
     </>
   )
 }
