91eb551c27ed0caacbaa236a097f5cc2e9293cbf
ulisescolosimo <colosimo101@gmail.com>
2022-09-07 13:05:13 -0300

    Llamamos a componente comments

--- src/pages/CityDetails.js
+++ src/pages/CityDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Details from '../components/Details'
 import Itinerary from '../components/Itinerary'
+import Comments from '../components/Comments'
 
 const CityDetails = () => {
   return (
     <>
         <Details />
         <Itinerary/>
+        <Comments />
     </>
   )
 }
