118ff113f7dc5006ba8b5b571f2da32287b82ec8
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-07 16:32:04 -0300

    correcciones minimas

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -4,9 +4,9 @@ import { useEffect, useState } from 'react';
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const Activities = () => {
+ const Activities = () => {
 
-            const { id } = useParams();
+           /*  const { id } = useParams();
           
             const [activity, setActivities] = useState() 
             
@@ -20,13 +20,12 @@ const Activities = () => {
               }
           
               useEffect(() => {
-                  fetchData();
-              }, []);
+                  fetchData(); /*  */
 
   return (
     <div className='container-activities'>
       <div className='container-activity' style={{background: `url(https://images.unsplash.com/photo-1662487845610-d3ae5e19a828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60)`}}>
-            <h3>{activity.name}</h3>
+            <h3>Pic</h3>
       </div>
       <div className='container-activity' style={{background: `url(https://images.unsplash.com/photo-1662487845610-d3ae5e19a828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60)`}}>
             <h3>Pic Eiffel Tower</h3>
@@ -36,7 +35,7 @@ const Activities = () => {
       </div>
       
     </div>
-  )
+  ) 
 }
 
 export default Activities
