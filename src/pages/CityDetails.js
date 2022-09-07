e4150f1b0606580a7d42937d1b35e08f897c1db6
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-07 13:14:23 -0300

    Agregado de componente 'Activities'

--- src/pages/CityDetails.js
+++ src/pages/CityDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Details from '../components/Details'
 import Itinerary from '../components/Itinerary'
+import Activities from '../components/Activities'
 
 const CityDetails = () => {
   return (
     <>
         <Details />
         <Itinerary/>
+        <Activities/>
     </>
   )
 }
