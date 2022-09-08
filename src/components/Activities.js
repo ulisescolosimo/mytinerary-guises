fcb615b368a700710315377f5867ea1dea245b00
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-07 23:39:55 -0300

    aplicacion de redux

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -10,18 +10,20 @@ import { useGetActivitiesQuery } from '../features/activitiesAPI'
 
       return (
         <>
-            { activitiesEach ?
+            { activitiesEach?.length > 0 ?
                 activitiesEach?.map((activity)=>{
                     return (
                         <div className='container-activities' key={activity._id}>
                           <div className='container-activity' style={{background: `url(https://images.unsplash.com/photo-1662487845610-d3ae5e19a828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60)`}}>
-                            <h3>{activity.name}</h3>
+                            <p>{activity.name}</p>
                           </div>
                         </div>
                     )
                 })
                 :
-                <p>Still no have Activities</p>
+                <div className="no-activities">
+                  <p>This itinerary has no activities yet!</p>
+                </div>
             }
         </>
       )
