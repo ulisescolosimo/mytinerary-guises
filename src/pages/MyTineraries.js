1a9c9db69d4cba450137fc08e426be2520ab2746
ulisescolosimo <colosimo101@gmail.com>
2022-09-08 02:00:00 -0300

    Nueva pagina mytineraries

---
+++ src/pages/MyTineraries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyTinerary from '../components/MyTinerary'
+
+const myTineraries = () => {
+  return (
+    <div>
+        <MyTinerary />
+    </div>
+  )
+}
+
+export default myTineraries
