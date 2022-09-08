cb8c27165a2c58e2fd96abbaafd92310cf571472
ulisescolosimo <colosimo101@gmail.com>
2022-09-08 17:39:09 -0300

    Fix un solo usuario por todos los itinerarios

--- src/components/MyTinerary.js
+++ src/components/MyTinerary.js
@@ -22,16 +22,15 @@ const MyTinerary = () => {
             <div className='container-mytiTitle'>
                 <h2>My Tineraries</h2>
             </div>
+            <div className="container-user-iti">
+                <p>{myitinerariesDetail?.[0].user.name}</p>
+                <img src={myitinerariesDetail?.[0].user?.photo} />
+            </div>
             <div className='container-mytinerary' >
                 { myitinerariesDetail ?
                     myitinerariesDetail?.map((item)=>{
                         return (<>
-                            <div className="container-user-iti">
-                                <p>User: {item.user.name}</p>
-                                <img src={item.user.photo} />
-                            </div>
                             <div className="container-myti">
-                                <div className='container-mytiText' >
                                     <div className='itinerary-div-p myItineraries-text' >
                                         <h3 className="Itinerary-p"> {item.name} </h3>
                                         <div className='text-itinerary'>
@@ -40,7 +39,8 @@ const MyTinerary = () => {
                                         <p className="Itinerary-p">  {item.likes}  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                     </svg> </p>
-                                        <p className="Itinerary-p"> {item.tags} </p>
+                                        <div className='container-tags'>
+                                            {item?.tags?.map((item) => <span className="tags">{item}</span>)}
                                         </div>
                                     </div>
                                 </div>
