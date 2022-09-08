65a5752db4e2c269a92c6269a44f6b0c1d0b6d28
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-07 23:57:39 -0300

    importaciones de apis

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import citiesApi  from './features/citiesApi'
 import activityAPI  from './features/activitiesAPI'
+import itineraryAPI from './features/itineraryAPI'
 
 export default configureStore({
   reducer: {
     [citiesApi.reducerPath] : citiesApi.reducer,
     [activityAPI.reducerPath]: activityAPI.reducer,
+    [itineraryAPI.reducerPath]: itineraryAPI.reducer,
   },
   middleware: (getAllCities) => getAllCities({
       immutableCheck: false,
