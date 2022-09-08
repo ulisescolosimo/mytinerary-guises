416d0ec1c2178ce8ee188a6208905e6f346cbdd0
ulisescolosimo <colosimo101@gmail.com>
2022-09-08 01:58:52 -0300

    Nuevo reducer en store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import citiesApi  from './features/citiesApi'
 import activityAPI  from './features/activitiesAPI'
 import itineraryAPI from './features/itineraryAPI'
+import myTineraryAPI from './features/myTineraryAPI'
 
 export default configureStore({
   reducer: {
     [citiesApi.reducerPath] : citiesApi.reducer,
     [activityAPI.reducerPath]: activityAPI.reducer,
     [itineraryAPI.reducerPath]: itineraryAPI.reducer,
+    [myTineraryAPI.reducerPath]: myTineraryAPI.reducer,
   },
   middleware: (getAllCities) => getAllCities({
       immutableCheck: false,
