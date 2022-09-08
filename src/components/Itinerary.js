9982bdc9d510a7389fe695d6b39a9e1bebf7fc67
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-07 23:41:46 -0300

    renderizado de itinerario

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -2,60 +2,41 @@ import React from 'react'
 import '../styles/Itinerary.css'
 import Activities from '../components/Activities'
 import Comments from '../components/Comments'
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react';
-import axios from 'axios'
 
+const Itinerary = ({city}) => {
 
-const Itinerary = ({itinerary}) => {
-
-      let data = Array.from(itinerary)
-
-      console.log(data);
-
-      const [activities, setActivities] = useState({})
-
-      useEffect(() =>{
-                  async function fetchActivities() {
-                        const acts = await axios.get(`http://localhost:4000/activities/`)
-                        setActivities(acts.data.response)
-                  }
-                        fetchActivities();
-                  }, []);
+      console.log(city);
 
       return (
             <div className='container-detail'>
-                  {data.map((item) => (
+                  {city?.map((item) => (
                         <>
                         <div className='container-card'>                      
                         <div className='container-itinerary' style={{background: 'linear-gradient(rgba(0,0,0,.85),rgba(0,0,0,.5)))'}}>
                               <div className='container-name'>
-                                    <h2>{item.name}</h2>                      
+                                    <p>{item?.name}</p>                      
                               </div>
                               <div className='container-price'>
-                                    <h4>Price: ${item.price}</h4>
+                                    <p>Price: ${item?.price}</p>
                               </div>                                                      
                               <div className='container-duration'>
-                                    <h4>Duration: {item.duration} minutes</h4>
+                                    <p>Duration: {item?.duration} minutes</p>
                               </div>
                               <div className='container-likes'>
-                                    <h4>{item.likes} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <p>{item?.likes} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                                    </svg></h4>
+                                    </svg></p>
                               </div>   
                               <div className='container-tags'>
-                                    <h4>{item.tags.map((item) => <span className="tags">{item}</span>)}</h4>
+                                    {item?.tags?.map((item) => <span className="tags">{item}</span>)}
                               </div>
                         </div>
                         <div className='container-profile'>    
                               <div className='container-user'>
-                                    <h3>{item.user.name}</h3>  
-                              </div>
-                              <div className='container-id'>
-                                    <h3>ID: {item.user._id}</h3>  
+                                    <p className="name-user">{item?.user?.name}</p>  
                               </div>
                               <div className='imgProfile'>
-                                    
+                                    <img className="img-user" style={{height: '100%', width: '100%'}} src={item?.user?.photo} />
                               </div>
                         </div>
                   </div>
