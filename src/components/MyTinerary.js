f4935feaddfe59c6cfc2f751cb8286409c911a72
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-08 06:53:37 -0300

    modificacion de estructura del return

--- src/components/MyTinerary.js
+++ src/components/MyTinerary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/MyTinerary.css'
 import { useGetItinerariesUserQuery } from '../features/myTineraryAPI'
 
 const MyTinerary = () => {
@@ -17,38 +18,40 @@ const MyTinerary = () => {
 
   return (
     <>
-        <div className='Mytineraries-div-title'>
-            <h2>My Tineraries</h2>
-        </div>
-        <div className='MyTineraries-div' >
-            { myitinerariesDetail ?
-                myitinerariesDetail?.map((item)=>{
-                    return (
-                        <div className="Itinerary-detail">
-                            <div className='itinerary-div-img' >
-                                <img src={item?.photo} className="Itinerary-img" alt={item.city} />
-                            </div>
-                            <div className='itinerary-div-text-activities' >
+        <div className='container-mytiGeneral'>
+            <div className='container-mytiTitle'>
+                <h2>My Tineraries</h2>
+            </div>
+            <div className='container-mytinerary' >
+                { myitinerariesDetail ?
+                    myitinerariesDetail?.map((item)=>{
+                        return (
+                            <div className="container-myti">
+                                <div className='container-mytiText' >
+                                    <div className='itinerary-div-p myItineraries-text' >
+                                        <h3 className="Itinerary-p"> {item.name} </h3>
+                                        <div className='text-itinerary'>
+                                        <p className="Itinerary-p"> Duration: {item.duration} minutes </p>
+                                        <p className="Itinerary-p"> Price: $ {item.price} </p>
+                                        <p className="Itinerary-p">  {item.likes}  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                    </svg> </p>
+                                        <p className="Itinerary-p"> {item.tags} </p>
+                                        </div>
 
-                                <div className='itinerary-div-p myItineraries-text' >
-                                    <h3 className="Itinerary-p"> {item.name} </h3>
-                                    <div className='text-itinerary'>
-                                    <p className="Itinerary-p"> Duration: {item.duration} </p>
-                                    <p className="Itinerary-p"> Price: $ {item.price} </p>
                                     </div>
+
 
                                 </div>
 
-
                             </div>
-
-                        </div>
-                        
-                    )
-                })
-                : <p> You dont have Itineraries yet</p>
-            } 
-        </div>
+                            
+                        )
+                    })
+                    : <p> You dont have Itineraries yet</p>
+                } 
+            </div>
+        </div>    
     </>
     
   )
