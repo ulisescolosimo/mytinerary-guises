090ef5be3cc77d889e12a6b241a62b8ab301c343
ulisescolosimo <colosimo101@gmail.com>
2022-09-12 16:53:51 -0300

    Creamos pagina para logueo

---
+++ src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignUpGoogle from '../components/SignUpGoogle'
+
+
+const SignUp = () => {
+  return (
+    <div>
+        <SignUpGoogle />
+    </div>
+  )
+}
+
+export default SignUp
