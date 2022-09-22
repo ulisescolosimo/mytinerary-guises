a67cf4cc33639ae03c605330dfd675955e4fe2ff
ulisescolosimo <colosimo101@gmail.com>
2022-09-22 16:03:10 -0300

    Fix likes length

--- src/components/MyTinerary.js
+++ src/components/MyTinerary.js
@@ -34,7 +34,7 @@ return (
                                         <div className='text-itinerary'>
                                         <p className="Itinerary-p"> Duration: {item.duration} minutes </p>
                                         <p className="Itinerary-p"> Price: $ {item.price} </p>
-                                        <p className="Itinerary-p">  {item.likes}  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                        <p className="Itinerary-p">  {item.likes.length}  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                     </svg> </p>
                                         <div className='container-tags' style={{display:'flex', flexWrap:'wrap', justifyContent:'center', alignItems:"center"}}>
